92d40fc534237a7cec455443ccddc8433ccd2b8d
raekwon_killop1603 <1034155@hr.nl>
2023-03-16 16:46:55 +0100

    Added assigment 2

--- Opdracht3/opdracht3.hs
+++ Opdracht3/opdracht3.hs
@@ -1,7 +1,15 @@
 module Opdracht3 where
+    import Data.List (group)
     
     --1a
     differentieer :: (Double -> Double) -> Double -> Double -> Double
     differentieer f p x = (f (x + p) - f (x - p)) / (2 * p)
 
 
+
+    --1b
+
+
+    --2
+    dubbelen :: Eq a => [a] -> [a]
+    dubbelen xs = concat $ filter (\g -> length g > 1) (group xs)
