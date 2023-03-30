6195ffc6a4a314f0d2e9ed8b4a5aa0d507fc6b8d
SabriDemir123 <sabri_demir@live.nl>
2023-03-30 16:38:49 +0200

    clean up

--- Opdracht3/opdracht3.hs
+++ Opdracht3/opdracht3.hs
@@ -1,4 +1,3 @@
-
 module Opdracht3 where
     import Data.List(group,nub)
     --1a
@@ -9,13 +8,12 @@ module Opdracht3 where
     -- Riemann left sum algorithm
     integreer::(Double -> Double) -> Double -> Double -> Double -> Double
     integreer f a b p =
-        let dx = (b - a) / (1/ p)
+        let dx = (b - a) / (1 / p)
         in dx * (sum [f x | x <- [a,a+dx..b-dx]])
 
 
 
     --2
     dubbelen' :: (Eq a) => [a] -> [a]
-    dubbelen' s = nub $ concat $ filter ((>1)  . length) $ group  s
-
+    dubbelen' s = nub $ concat $ filter ((>1) . length) $ group s
 
