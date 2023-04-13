336afa0fcba0b6cb15025f8535851143dfaf7836
JKachoura <jaouadkachoura@hotmail.com>
2023-04-13 15:22:20 +0200

    opdracht 2 verbeterd

--- Opdracht3/opdracht3.hs
+++ Opdracht3/opdracht3.hs
@@ -1,6 +1,7 @@
 module Opdracht3 where
-    import Data.List(group,nub)
+    import Data.List(group,nub,(\\))
     --1a
+    -- Differentiequotient
     differentieer :: (Double -> Double) -> Double -> Double -> Double
     differentieer f p x = (f (x + p) - f (x)) / p
 
@@ -11,9 +12,8 @@ module Opdracht3 where
         let dx = (b - a) / (1 / p)
         in dx * (sum [f x | x <- [a,a+dx..b-dx]])
 
-
-
     --2
-    dubbelen' :: (Eq a) => [a] -> [a]
-    dubbelen' s = nub $ concat $ filter ((>1) . length) $ group s
+    -- List met element die meer dan 1 keer voorkomen
+    dubbelen::Eq a => [a] -> [a]
+    dubbelen s = nub (s \\ nub s)
 
